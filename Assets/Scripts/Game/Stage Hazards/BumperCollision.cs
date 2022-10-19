1ee4819a42d310dd580c0cd8c7856f1d175d648b
fmgamma <72016972+fmgamma@users.noreply.github.com>
2022-10-19 15:05:53 +0100

    Add Stage Hazards Files

---
+++ Assets/Scripts/Game/Stage Hazards/BumperCollision.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperCollision : MonoBehaviour
+{
+    [SerializeField] private Sprite bumperIdle = null;
+    [SerializeField] private Sprite bumperHit = null;
+    private SpriteRenderer bumperSprite;
+    private float hitCooldown;
+
+    void Start()
+    {
+        bumperSprite = GetComponent<SpriteRenderer>();
+        hitCooldown = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hitCooldown > 0)
+        {
+            bumperSprite.sprite = bumperHit;
+            hitCooldown -= Time.deltaTime;
+        }
+        else
+        {
+            bumperSprite.sprite = bumperIdle;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Vector2 playerPos = new Vector2 (collision.collider.transform.position.x, collision.collider.transform.position.y);
+            Vector2 posDifference = playerPos - new Vector2(transform.position.x, transform.position.y);
+            Rigidbody2D playerBody = collision.collider.GetComponent<Rigidbody2D>();
+            Vector2 bumperScale = new Vector2(gameObject.transform.localScale.x, gameObject.transform.localScale.y);
+            playerBody.AddForce(posDifference.normalized * bumperScale * 12, ForceMode2D.Impulse);;
+
+            hitCooldown = 0.15f;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Game/Stage Hazards/FlipperManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperManager : MonoBehaviour
+{
+    [SerializeField] private Sprite flipperIdle = null;
+    [SerializeField] private Sprite flipperActivated = null;
+
+    private SpriteRenderer flipperSprite;
+    private float flipperCooldown;
+    private float flipperAnimation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flipperSprite = GetComponent<SpriteRenderer>();
+        flipperCooldown = Random.Range(1.0f, 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flipperAnimation > 0)
+        {
+            flipperAnimation -= Time.deltaTime;
+            flipperSprite.sprite = flipperActivated;
+        }
+        else
+        {
+            flipperSprite.sprite = flipperIdle;
+        }
+
+        flipperCooldown -= Time.deltaTime;
+        if (flipperCooldown <= 0)
+        {
+            flipperAnimation = 0.15f;
+            flipperCooldown = Random.Range(1.0f, 3.0f);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (flipperAnimation > 0 && collision.collider.CompareTag("Player"))
+        {
+            Vector2 bumpOffset = new Vector2 (transform.localPosition.x, transform.localPosition.y - 1.0f);
+            Vector2 playerPos = new Vector2(collision.collider.transform.position.x, collision.collider.transform.position.y);
+
+            Vector2 launchDirection = playerPos - bumpOffset;
+            Rigidbody2D playerBody = collision.collider.GetComponent<Rigidbody2D>();
+            playerBody.AddForce(launchDirection.normalized * 12, ForceMode2D.Impulse);
+        }
+    }
+
+}
